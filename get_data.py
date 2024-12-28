6c255617844a7a7c61228382d52b6350ae66ee64
Hung-En Liao <Hung-En Liao>
2024-12-28 14:55:38 -0400

    Data folder

--- get_data.py
+++ get_data.py
@@ -4,8 +4,8 @@ import streamlit as st # type: ignore
 
 @st.cache_data
 def get_data():
-    file_1 = "bike_sales.csv"
-    file_2 = "Salesperson_name.csv"
+    file_1 = "data/bike_sales.csv"
+    file_2 = "data/Salesperson_name.csv"
     db1 = pd.read_csv(file_1)
     db2 = pd.read_csv(file_2)
     df = pd.merge(db1, db2, on='Salesperson_ID', how='inner')
